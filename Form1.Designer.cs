98c374b931b954c145f0e4da18254b3b1a4b1b6a
DenisNguyen <vduyit@gmail.com>
2023-06-08 16:23:54 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace QuanLyKhachSan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTenTaiKhoan = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtTenTaiKhoan
+            // 
+            this.txtTenTaiKhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtTenTaiKhoan.Location = new System.Drawing.Point(418, 280);
+            this.txtTenTaiKhoan.Multiline = true;
+            this.txtTenTaiKhoan.Name = "txtTenTaiKhoan";
+            this.txtTenTaiKhoan.Size = new System.Drawing.Size(261, 49);
+            this.txtTenTaiKhoan.TabIndex = 1;
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtMatKhau.Location = new System.Drawing.Point(418, 367);
+            this.txtMatKhau.Multiline = true;
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.PasswordChar = '*';
+            this.txtMatKhau.Size = new System.Drawing.Size(261, 49);
+            this.txtMatKhau.TabIndex = 2;
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnDangNhap.Location = new System.Drawing.Point(564, 446);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(115, 42);
+            this.btnDangNhap.TabIndex = 3;
+            this.btnDangNhap.Text = "Đăng nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::QuanLyKhachSan.Properties.Resources.eye_icon_vector_illustration;
+            this.pictureBox4.Location = new System.Drawing.Point(629, 367);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(38, 49);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::QuanLyKhachSan.Properties.Resources.Icons8_Windows_8_Security_Password_2__2_;
+            this.pictureBox3.Location = new System.Drawing.Point(312, 367);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(58, 49);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::QuanLyKhachSan.Properties.Resources.user_icon_150670;
+            this.pictureBox2.Location = new System.Drawing.Point(312, 280);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(58, 49);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::QuanLyKhachSan.Properties.Resources._0af3c9613761d2d2394d99312aeba397;
+            this.pictureBox1.Location = new System.Drawing.Point(312, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(367, 225);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnDangNhap;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(958, 569);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.btnDangNhap);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.txtTenTaiKhoan);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox txtTenTaiKhoan;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Net.NetworkInformation;
+
+namespace QuanLyKhachSan
+{
+    public partial class Form1 : Form
+    {   
+        // Khai báo một biến để lưu đường dẫn hình ảnh con mắt mở
+        private string eyeOpenImagePath = "C:\\Users\\nvduy\\Downloads\\eye-icon-vector-illustration.jpg";
+
+        // Khai báo một biến để lưu đường dẫn hình ảnh con mắt đóng
+        private string eyeCloseImagePath = "C:\\Users\\nvduy\\Downloads\\th.jpg";
+
+        private void TogglePasswordVisibility()
+        {
+            if (txtMatKhau.UseSystemPasswordChar)
+            {
+                // Nếu đang hiển thị dạng password, thay đổi thành hiển thị dạng văn bản
+                txtMatKhau.UseSystemPasswordChar = false;
+                // Thay đổi hình ảnh của PictureBox thành hình ảnh con mắt đóng
+                pictureBox4.Image = Image.FromFile(eyeCloseImagePath);
+            }
+            else
+            {
+                // Ngược lại, nếu đang hiển thị dạng văn bản, thay đổi thành hiển thị dạng password
+                txtMatKhau.UseSystemPasswordChar = true;
+                // Thay đổi hình ảnh của PictureBox thành hình ảnh con mắt mở
+                pictureBox4.Image = Image.FromFile(eyeOpenImagePath);
+            }
+        }
+
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            string connectionString = @"Data Source=JAVIS\\SQLEXPRESS;Initial Catalog=QuanLyKhachSan;Integrated Security=True";
+            string tenTaiKhoan = txtTenTaiKhoan.Text;
+            string matKhau = txtMatKhau.Text;
+            if (tenTaiKhoan.Trim() == "" || matKhau.Trim() == "") { MessageBox.Show("Vui lòng nhập đầy đủ thông tin!"); }
+            // Thực hiện truy vấn vào cơ sở dữ liệu
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            string query = "SELECT LoaiTaiKhoan FROM TaiKhoan WHERE TenTaiKhoan = @TenTaiKhoan AND MatKhau = @MatKhau";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@TenTaiKhoan", tenTaiKhoan);
+            cmd.Parameters.AddWithValue("@MatKhau", matKhau);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                reader.Read();
+                string loaiTaiKhoan = reader["LoaiTaiKhoan"].ToString();
+
+                // Kiểm tra và xử lý theo vai trò của người dùng
+                if (loaiTaiKhoan == "QUANLY")
+                {
+                    // Xử lý với vai trò quản lý
+                }
+                else if (loaiTaiKhoan == "LETAN")
+                {
+                    // Xử lý với vai trò lễ tân
+                }
+                else if (loaiTaiKhoan == "ADMIN")
+                {
+                    // Xử lý với vai trò admin
+                    MessageBox.Show("dang nhap thanh cong");
+                }
+                else
+                {
+                    // Thông báo lỗi vai trò không hợp lệ
+                }
+            }
+            else
+            {
+                // Thông báo lỗi đăng nhập không thành công
+            }
+
+            reader.Close();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            TogglePasswordVisibility();
+        }
+    }
+}
